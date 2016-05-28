419d53ec4a0fe56e95637ca7b6fb106dd835f477
Chris <orthagonal1@gmail.com>
2016-05-28 14:30:10 -0500

    first checkin, will forward a message to slack channel on server.log

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+const _ = require('lodash');
+const Wreck = require('wreck');
+const urlParse = require('url-parse');
+
+exports.register = (server, config, next) => {
+  const url = urlParse(config.slackHook);
+  const baseUrl = `${url.protocol}//${url.hostname}`;
+  const path = url.pathname;
+  console.log(`will forward messages to ${baseUrl} and path ${path}`);
+  const postMessageToSlack = (tags, data) => {
+    const text = `[${_.keys(tags)}] ${data}`;
+    Wreck.request('POST', path, {
+      baseUrl : baseUrl,
+      headers: { 'Content-type': 'application/json' },
+      payload: JSON.stringify({ text })
+    });
+  };
+
+  // event that fires whenever server.log is called:
+  server.on('log', (event, tags) => {
+    if (_.intersection(_.keys(tags), config.tags).length > 0) {
+      postMessageToSlack(tags, event.data);
+    }
+  });
+  next();
+};
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
---
+++ test/server-test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Hapi = require('hapi');
+const code = require('code');
+const lab = exports.lab = require('lab').script();
+const hapiSlack = require('../');
+const _ = require('lodash');
+let server;
+
+const options = {
+  slackHook: 'https://hooks.slack.com/services/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',
+  tags: ['warning', 'error', 'test']
+}
+
+lab.beforeEach((done) => {
+  server = new Hapi.Server({});
+  server.connection({ port: 8080 });
+  server.register({
+    register: hapiSlack,
+    options
+  }, () => {
+    done();
+  });
+});
+
+lab.afterEach((done) => {
+  server.stop(() => {
+    done();
+  });
+});
+
+lab.test('posts to test slack channel ', (done) => {
+  try {
+    server.log(['warning', 'error'], 'this is a test post that should be posted to the channel');
+  } catch (e) {
+    console.log(e)
+  } finally {
+  }
+  _.delay(done, 5000)
+});
+lab.test('does not post when tags do not match ', (done) => {
+  try {
+    server.log(['asdf', 'asdf'], 'this should not be posted to the channel');
+  } catch (e) {
+    console.log(e)
+  } finally {
+  }
+  _.delay(done, 5000)
+});
