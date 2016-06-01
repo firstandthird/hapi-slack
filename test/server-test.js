192382460c5cbb2e75956603032c4e12aab1d4e4
Chris <orthagonal1@gmail.com>
2016-06-01 16:32:03 -0500

    second version
    -can send object as a message
    - objects containing a 'message' key will have that message pulled out
    - exposes call to send server.log styled messages to slack in
    server.methods
    - exposes call to send raw API data to slack in server.methods
    - 'error', 'warning' and 'success' tags are all colorized respectively
    -

--- test/server-test.js
+++ test/server-test.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const Hapi = require('hapi');
-const code = require('code');
 const lab = exports.lab = require('lab').script();
 const hapiSlack = require('../');
-const _ = require('lodash');
 let server;
 
 const options = {
@@ -14,7 +12,9 @@ const options = {
   // you can provide a name for any channel allowed by the above slack webhook:
   channel: '#hapi-slack-test',
   // you can list which tags will cause a server.log call to post to slack:
-  tags: ['warning', 'error', 'test']
+  tags: ['warning', 'error', 'success', 'test'],
+  // you can specify tags that will automatically be appended to each post to slack:
+  additionalTags: ['server-test.js', 'someAdditionalTag']
 };
 
 lab.beforeEach((done) => {
@@ -34,23 +34,38 @@ lab.afterEach((done) => {
   });
 });
 lab.test('posts to test slack channel ', (done) => {
-  try {
-    server.log(['warning', 'error'], 'this is a test post from hapi-slack.  Just ignore it.');
-  } catch (e) {
-    console.log(e)
-  } finally {
-  }
-  _.delay(done, 2000)
+  server.log(['test'], 'this is a simple test post from hapi-slack.');
 });
-lab.test('does not post when tags do not match ', (done) => {
-  server.log(['asdf', 'asdf'], 'this should not be posted to the channel');
-  _.delay(done, 2000)
-});
-lab.test('lets you call the post method manually', (done) => {
-  server.methods.postMessageToSlack(['test', 'postMessageToSlack'], 'testing server.method.postMessageToSlack.  Just ignore this.');
-  done();
+/*
+lab.test('posts to test slack channel with color ', (done) => {
+  server.log(['error', 'test'], 'this is a simple test post from hapi-slack that should have red next to it.');
 });
 lab.test('lets you post an object as the message', (done) => {
-  server.log(['error'], { text: 'message is object' });
-  _.delay(done, 2000)
+  server.log(['test'], { data: 'this is an object' });
 });
+lab.test('warning tags will have a yellow swatch', (done) => {
+  server.log(['warning'], { data: 'this is a warning object, should have yellow next to it' });
+});
+lab.test('error tags will have a red swatch', (done) => {
+  server.log(['error'], { data: 'this is an error object, should have red next to it' });
+});
+lab.test('success tags will have a green swatch', (done) => {
+  server.log(['success'], { data: 'this is an success object, should have green next to it' });
+});
+lab.test('lets you post an object with a special "message" field', (done) => {
+  server.log(['error'], { message: 'this is the message that was pulled out of the object below', data: 'this is an object and should be formatted' });
+});
+lab.test('does not post when tags do not match ', (done) => {
+server.log(['asdf', 'asdf'], 'this should not be posted to the channel');
+});
+lab.test('lets you call the post method manually', (done) => {
+  server.methods.postMessageToSlack(['test', 'postMessageToSlack'], 'this is a test of server.method.postMessageToSlack. ');
+});
+lab.test('lets you call the raw post method manually', (done) => {
+  server.methods.postRawDataToSlack({ text: 'this is a test of server.method.postRawDataToSlack .' });
+});
+
+lab.test('will not process tags that have "hapi-slack"', (done) => {
+  server.log(['hapi-slack', 'error'], 'this should not be posted to the channel');
+});
+*/
